1b44de98a87dd429c175f8a3aa5fd487f2137013
fadeldnswr <fadel.achmad@ui.ac.id>
2025-07-10 11:56:47 +0700

    solve the investment problem

--- investment.py
+++ investment.py
@@ -3,29 +3,31 @@ from pyomo.opt import SolverFactory
 
 m = pyo.ConcreteModel()
 
-#sets and paramenters
+# Initialize the model
+# Define the set of investments and the total capital available
 m.setInv = pyo.Set(initialize=['A','B','C'])
 m.Capital = 100000
 
-#variables
-m.C = pyo.Var(m.setInv, bounds=(0,None))
-m.R = pyo.Var(m.setInv, bounds=(0,None))
+# Define variables
+m.C = pyo.Var(m.setInv, bounds=(0,None)) # Capital fund allocated to each investment
+m.R = pyo.Var(m.setInv, bounds=(0,None)) # RoI for each investment
 
-#objective function
+# Define the objective function
+# Objective : Maximize the total return on investment
 m.obj = pyo.Objective(expr = pyo.summation(m.R), sense=pyo.maximize)
 
-#constraints
-m.C1 = pyo.Constraint(expr = pyo.summation(m.C) == m.Capital)
-m.C2 = pyo.Constraint(expr = m.R['A'] == 0.05*m.C['A'])
-m.C3 = pyo.Constraint(expr = m.R['B'] == 0.10*m.C['B'])
-m.C4 = pyo.Constraint(expr = m.R['C'] == 0.12*m.C['C'])
-m.C5 = pyo.Constraint(expr = m.C['B'] <= 0.2*m.Capital)
-m.C6 = pyo.Constraint(expr = m.C['C'] <= 0.1*m.Capital)
+# Define constraints
+m.C1 = pyo.Constraint(expr = pyo.summation(m.C) == m.Capital) # Total capital allocated must be equal to the available capital
+m.C2 = pyo.Constraint(expr = m.R['A'] == 0.05*m.C['A']) # RoI for investment A is 5% of capital allocated to A
+m.C3 = pyo.Constraint(expr = m.R['B'] == 0.10*m.C['B']) # RoI for investment B is 10% of capital allocated to B
+m.C4 = pyo.Constraint(expr = m.R['C'] == 0.12*m.C['C']) # RoI for investment C is 12% of capital allocated to C
+m.C5 = pyo.Constraint(expr = m.C['B'] <= 0.2*m.Capital) # Capital allocated to investment B must not exceed 20% of total capital
+m.C6 = pyo.Constraint(expr = m.C['C'] <= 0.1*m.Capital) # Capital allocated to investment C must not exceed 10% of total capital
 
-#solve
+# Solve the model optimization problem
 opt = SolverFactory('gurobi')
 m.results = opt.solve(m)
 
-#print
+# Display the results
 m.pprint()
 print('\n\nOF:',pyo.value(m.obj))
